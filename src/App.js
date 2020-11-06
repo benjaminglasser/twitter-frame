0a4b6913e7809ee4c6b8d8e4355f4552e28b3826
ben glasser <ben@fantasticvoyage.com>
2020-11-05 16:49:17 -0800

    show all tweets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
+import {
+  useState,
+  useEffect,
+ } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const initialTweets = [
+]
 
 function App() {
+  const [tweets, setTweets] = useState(initialTweets)
+  const [current, setCurrent] = useState(0)
+  const [loading, setLoading] = useState(false)
+  
+  // useEffect(() => {
+  //   rehydrateTweets()
+  // },[])
+
+  function rehydrateTweets() {
+    axios.get('http://localhost:8000/latest')
+      .then((response) => {
+        setTweets(response.data);
+        setLoading(false)
+      })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     { loading
+        ? <div className="loader">Loading tweets...</div>
+        : tweets.map((tweet, index) => {
+            return(
+              <div key={index}>
+                {tweet.text}
+              </div>
+            )
+          })
+     }
+     <button onClick={e => {
+       setCurrent(current+1)
+     }}>next</button>
+     <button onClick={e => {
+       setLoading(true)
+       rehydrateTweets()
+     }}>Refresh Tweets</button>
     </div>
   );
 }
